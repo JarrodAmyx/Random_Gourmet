70cc002cc3f5d22c3840161f1c9d7cc21d757021
JoakimEckerman <joakim.eckerman01@student.csulb.edu>
2023-09-14 13:21:07 -0700

    Started work on User Security and Authentication

--- src/main/java/com/randomgourmet/randomgourmet/services/UserService.java
+++ src/main/java/com/randomgourmet/randomgourmet/services/UserService.java
@@ -3,6 +3,10 @@ package com.randomgourmet.randomgourmet.services;
 import com.randomgourmet.randomgourmet.models.User;
 import com.randomgourmet.randomgourmet.dao.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -72,6 +76,69 @@ public class UserService {
         return passwordEncoder.encode(password);
     }
 
+    public boolean isPasswordComplex(String password) {
+        // Implement password complexity validation logic here.
+        // Example: Check if the password has at least 8 characters and contains a mix of letters, digits, and special characters.
+        return password.matches("^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[@#$%^&+=!]).{8,}$");
+    }
+
+    public void lockUserAccount(String userId) {
+        // Implement logic to lock a user's account temporarily after multiple failed login attempts.
+        // You can update the user's account status or set a flag in the user's profile.
+    }
+
+    public void unlockUserAccount(String userId) {
+        // Implement logic to unlock a user's previously locked account.
+        // Remove any locks or flags that were set during account locking.
+    }
+
+    public boolean authenticateUser(String username, String password) {
+        // Implement logic to authenticate a user based on their username and password.
+        // Example: Use Spring Security's authentication manager.
+        try {
+            Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(username, password)
+            );
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            return true;
+        } catch (AuthenticationException e) {
+            return false;
+        }
+    }
+
+    public void registerUser(User user) {
+        // Implement logic to register a new user, including validation and account creation.
+        // Example: Save the user in the database after validation.
+        validateUser(user);
+        user.setPassword(hashPassword(user.getPassword()));
+        userRepository.save(user);
+    }
+
+    public void logoutUser(String userId) {
+        // Implement logic to log out a user by invalidating their session or access token.
+        // Example: Use Spring Security's logout functionality.
+        SecurityContextHolder.clearContext();
+    }
+
+    public void deleteUserProfile(String userId) {
+        // Implement logic to allow users to delete their profiles, including associated data.
+        // Example: Delete the user's data from the database.
+        userRepository.deleteById(userId);
+    }
+
+    public void logUserActivity(String userId, String activity) {
+        // Implement logging of user activities for auditing purposes.
+        // Example: Log user activity to an audit log or database table.
+        UserActivityLog log = new UserActivityLog();
+        log.setUserId(userId);
+        log.setActivity(activity);
+        userActivityLogRepository.save(log);
+    }
+
+    // Additional utility methods and error handling should be included.
+
+    // ...
+}
     // Implement other methods as needed for user management, authentication, and authorization.
 
     // ...
