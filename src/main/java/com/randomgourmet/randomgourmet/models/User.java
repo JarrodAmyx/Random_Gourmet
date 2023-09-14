4a679136caf4b6602ce94a5c2febee5d1fa2011a
JoakimEckerman <joakim.eckerman01@student.csulb.edu>
2023-09-13 19:21:54 -0700

    Update User.java
    add password and getters/setters

--- src/main/java/com/randomgourmet/randomgourmet/models/User.java
+++ src/main/java/com/randomgourmet/randomgourmet/models/User.java
@@ -10,6 +10,39 @@ public class User {
     private String id;
     private String username;
     private String email;
+    private String password;
 
     // Getters and setters
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
