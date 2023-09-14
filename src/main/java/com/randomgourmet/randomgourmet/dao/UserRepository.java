1f353d15fae243073efafeaa2824c37b79405151
JoakimEckerman <joakim.eckerman01@student.csulb.edu>
2023-09-13 19:37:23 -0700

    User, UserService, UserRepository work

--- src/main/java/com/randomgourmet/randomgourmet/dao/UserRepository.java
+++ src/main/java/com/randomgourmet/randomgourmet/dao/UserRepository.java
@@ -3,7 +3,25 @@ package com.randomgourmet.randomgourmet.dao;
 
 import com.randomgourmet.randomgourmet.models.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
+@Repository
 public interface UserRepository extends MongoRepository<User, String> {
-    // Custom query methods can be defined here
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+
+    List<User> findByOrderByUsernameAsc();
+
+    @Query("{ 'username' : ?0, 'email' : ?1 }")
+    Optional<User> findByUsernameAndEmail(String username, String email);
+
+    // Custom update query using @Query annotation
+    @Query("{ '_id' : ?0 }")
+    void updateUserPassword(String userId, String newPassword);
 }
--- src/main/java/com/randomgourmet/randomgourmet/services/UserService.java
+++ src/main/java/com/randomgourmet/randomgourmet/services/UserService.java
@@ -3,21 +3,77 @@ package com.randomgourmet.randomgourmet.services;
 import com.randomgourmet.randomgourmet.models.User;
 import com.randomgourmet.randomgourmet.dao.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public User createUser(User user) {
+        // Implement user creation logic here (e.g., validation, hashing passwords).
+        validateUser(user); // Example validation method
+        user.setPassword(hashPassword(user.getPassword())); // Hash the password
         return userRepository.save(user);
     }
 
-    // Other service methods for user management
+    public Optional<User> getUserById(String userId) {
+        return userRepository.findById(userId);
+    }
+
+    public Optional<User> getUserByUsername(String username) {
+        // Implement logic to find a user by username.
+        return userRepository.findByUsername(username);
+    }
+
+    public Optional<User> getUserByEmail(String email) {
+        // Implement logic to find a user by email.
+        return userRepository.findByEmail(email);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public void updateUser(User user) {
+        // Implement logic to update user information.
+        userRepository.save(user);
+    }
+
+    public void changePassword(User user, String newPassword) {
+        // Implement logic to change a user's password.
+        user.setPassword(hashPassword(newPassword)); // Hash the new password
+        userRepository.save(user);
+    }
+
+    public void deleteUser(String userId) {
+        // Implement logic to delete a user by their ID.
+        userRepository.deleteById(userId);
+    }
+
+    // Validate user data before creation
+    private void validateUser(User user) {
+        // Implement validation logic (e.g., check for unique username and email).
+        // Throw exceptions or handle validation errors as needed.
+    }
+
+    // Hash user password using BCrypt
+    private String hashPassword(String password) {
+        return passwordEncoder.encode(password);
+    }
+
+    // Implement other methods as needed for user management, authentication, and authorization.
+
+    // ...
 
 }
