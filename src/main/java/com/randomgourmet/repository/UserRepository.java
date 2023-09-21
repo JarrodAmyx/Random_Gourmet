a5490159b52080b3a27e814ddfe1315118aace08
Brian Tran <ninjapika500@outlook.com>
2023-09-20 19:14:36 -0700

    Revert UserRepo back to <User,String> as seen in order tutorials

--- src/main/java/com/randomgourmet/repository/UserRepository.java
+++ src/main/java/com/randomgourmet/repository/UserRepository.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 
 
 @Repository
-public interface UserRepository extends MongoRepository<User, Long> { // Temp change from <User, String> to <User, Long>
+public interface UserRepository extends MongoRepository<User, String> { 
 
     Optional<User> findByUsername(String username);
 
