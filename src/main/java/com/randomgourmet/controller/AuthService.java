bcd335fa33bba5e825df90758cc2dec23edf442a
Brian Tran <ninjapika500@outlook.com>
2023-09-20 19:06:55 -0700

    Stopped autowire errors

--- src/main/java/com/randomgourmet/controller/AuthService.java
+++ src/main/java/com/randomgourmet/controller/AuthService.java
@@ -1,6 +1,8 @@
 package com.randomgourmet.controller;
 
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.*;
 
 @Service
