f050ebf729a82f8e95a780e434790bd454a714d9
Ryan Lertpichitkul <38081512+rlert@users.noreply.github.com>
2023-09-23 22:49:34 -0700

    Added search bar
    - implemented search bar to search through saved recipes
    
    todo:
    - option to delete saved recipe

--- gourmet-angular/src/app/profile/profile.component.ts
+++ gourmet-angular/src/app/profile/profile.component.ts
@@ -10,10 +10,37 @@ export class ProfileComponent {
   username: string = 'TestUser124';
   email: string = 'test@email.com';
 
+  searchTerm: string = '';
+  searchResults: string[] = [];
+  searchBool: boolean = false;
+
+  // random stuff for testing, needs to be called from backend later
   recipes: string[] = ['burger', 'thing', 'thing2'];
 
 
-  clickRecipe(other: string){
-    console.log(other);
+  // function to 
+  clickRecipe(other: string): void{
+    // placeholder for API/database call
+    console.log(other); 
+  }
+
+  searchRecipe(): void{
+    // skips search process if search term is empty
+    if(this.searchTerm.length === 0){
+      this.searchBool = false;
+      return;
+    }
+
+    // empties the list
+    this.searchResults = [];
+    this.searchBool = true;
+
+    this.recipes.forEach(recipe => {
+      if(recipe.includes(this.searchTerm)){
+        this.searchResults.push(recipe);
+      }
+    });
+
+    console.log(this.searchResults);
   }
 }
