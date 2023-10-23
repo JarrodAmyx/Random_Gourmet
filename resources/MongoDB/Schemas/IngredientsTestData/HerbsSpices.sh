506e5a629bf503af1fd41e71af3467b5d30e5850
chelcx2 <100275141+chelcx2@users.noreply.github.com>
2023-10-23 13:21:51 -0700

    ingredients table w all test data successfully in mongodb w test queries for validation

---
+++ resources/MongoDB/Schemas/IngredientsTestData/HerbsSpices.sh
@@ -0,0 +1,212 @@
+db.ingredients.insertMany([
+  {
+    "name": "Salt",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Herbs and Spices"
+  },
+  {
+    "name": "Pepper",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Herbs and Spices"
+  },
+  {
+    "name": "Basil",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Herbs and Spices"
+  },
+  {
+    "name": "Thyme",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Herbs and Spices"
+  },
+  {
+    "name": "Rosemary",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Herbs and Spices"
+  },
+  {
+    "name": "Oregano",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Herbs and Spices"
+  },
+  {
+    "name": "Cilantro",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Herbs and Spices"
+  },
+  {
+    "name": "Parsley",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Herbs and Spices"
+  },
+  {
+    "name": "Sage",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Herbs and Spices"
+  },
+  {
+    "name": "Dill",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Herbs and Spices"
+  },
+  {
+    "name": "Mint",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Herbs and Spices"
+  },
+  {
+    "name": "Chive",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Herbs and Spices"
+  },
+  {
+    "name": "Coriander",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Herbs and Spices"
+  },
+  {
+    "name": "Cumin",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Herbs and Spices"
+  },
+  {
+    "name": "Paprika",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Herbs and Spices"
+  },
+  {
+    "name": "Chili Powder",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Herbs and Spices"
+  },
+  {
+    "name": "Cayenne Pepper",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Herbs and Spices"
+  },
+  {
+    "name": "Turmeric",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Herbs and Spices"
+  },
+  {
+    "name": "Ginger",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Herbs and Spices"
+  },
+  {
+    "name": "Nutmeg",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Herbs and Spices"
+  },
+  {
+    "name": "Cinnamon",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Herbs and Spices"
+  },
+  {
+    "name": "Clove",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Herbs and Spices"
+  },
+  {
+    "name": "Allspice",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Herbs and Spices"
+  },
+  {
+    "name": "Cardamom",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Herbs and Spices"
+  },
+  {
+    "name": "Bay Leaf",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Herbs and Spices"
+  },
+  {
+    "name": "Fennel",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Herbs and Spices"
+  },
+  {
+    "name": "Tarragon",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Herbs and Spices"
+  },
+  {
+    "name": "Marjoram",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Herbs and Spices"
+  },
+  {
+    "name": "Lavender",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Herbs and Spices"
+  },
+  {
+    "name": "Vanilla",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Herbs and Spices"
+  },
+  {
+    "name": "Saffron",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Herbs and Spices"
+  },
+  {
+    "name": "Mustard Seed",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Herbs and Spices"
+  },
+  {
+    "name": "Caraway Seed",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Herbs and Spices"
+  },
+  {
+    "name": "Poppy Seed",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Herbs and Spices"
+  },
+  {
+    "name": "Other Herb and Spice",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Herbs and Spices"
+  }
+]);
