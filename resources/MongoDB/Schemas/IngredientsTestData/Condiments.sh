198f124e7cdba8c2202c4dbdf6b4ea13fd1b7afc
chelcx2 <100275141+chelcx2@users.noreply.github.com>
2023-10-23 13:12:42 -0700

    additional table test data condiments, grainscereasl, baking, juices

---
+++ resources/MongoDB/Schemas/IngredientsTestData/Condiments.sh
@@ -0,0 +1,188 @@
+db.ingredients.insertMany([
+  {
+    "name": "Ketchup",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Condiments"
+  },
+  {
+    "name": "Mustard",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Condiments"
+  },
+  {
+    "name": "Mayonnaise",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Condiments"
+  },
+  {
+    "name": "Soy Sauce",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Condiments"
+  },
+  {
+    "name": "Hot Sauce",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Condiments"
+  },
+  {
+    "name": "Barbecue Sauce",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Condiments"
+  },
+  {
+    "name": "Worcestershire Sauce",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Condiments"
+  },
+  {
+    "name": "Vinegar",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Condiments"
+  },
+  {
+    "name": "Salad Dressing",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Condiments"
+  },
+  {
+    "name": "Relish",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Condiments"
+  },
+  {
+    "name": "Pickle",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Condiments"
+  },
+  {
+    "name": "Salsa",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Condiments"
+  },
+  {
+    "name": "Sriracha",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Condiments"
+  },
+  {
+    "name": "Hoisin Sauce",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Condiments"
+  },
+  {
+    "name": "Teriyaki Sauce",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Condiments"
+  },
+  {
+    "name": "Fish Sauce",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Condiments"
+  },
+  {
+    "name": "Tahini",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Condiments"
+  },
+  {
+    "name": "Hummus",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Condiments"
+  },
+  {
+    "name": "Jam",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Condiments"
+  },
+  {
+    "name": "Jelly",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Condiments"
+  },
+  {
+    "name": "Peanut Butter",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Condiments"
+  },
+  {
+    "name": "Nutella",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Condiments"
+  },
+  {
+    "name": "Gravy",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Condiments"
+  },
+  {
+    "name": "Marinara Sauce",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Condiments"
+  },
+  {
+    "name": "Pizza Sauce",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Condiments"
+  },
+  {
+    "name": "Tartar Sauce",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Condiments"
+  },
+  {
+    "name": "Cocktail Sauce",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Condiments"
+  },
+  {
+    "name": "Horseradish",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Condiments"
+  },
+  {
+    "name": "Sour Cream",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Condiments"
+  },
+  {
+    "name": "Cream Cheese",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Condiments"
+  },
+  {
+    "name": "Other Condiment",
+    "quantity": 0,
+    "unit": "0",
+    "category": "Condiments"
+  }
+]);
