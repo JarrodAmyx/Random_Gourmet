9b2edffd3945bcc23c9fb809e3cf1af76babd58a
root <root@ip-172-31-2-238.us-west-1.compute.internal>
2023-11-04 04:20:58 +0000

    Add backend CRUD methods to API

---
+++ Laravel-Backend/app/Http/Controllers/UserRecipes.php
@@ -0,0 +1,88 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use MongoDB\BSON\ObjectId;
+
+use Illuminate\Http\Request;
+
+class UserRecipes extends Controller
+{
+    public function create( Request $request )
+    {
+        $userId = $request->userId; //'john@example.com'
+        $recipeId = $request->recipeId;
+
+        $userRecipe =
+            DB::connection('mongodb')
+                ->collection('userRecipes')
+                ->where('userId', $userId)
+                ->where('recipeId', $recipeId)
+                ->first()
+        ;
+
+        if( !$userRecipe )
+        {
+            $data = [
+                'userId' => $userId,
+                'recipeId' => $recipeId
+            ];
+
+            DB::connection('mongodb')->collection('userRecipes')->insert($data);
+
+            return response()->json(['message' => 'Recipe Successfully Added To User']);
+        }
+        
+        return response()->json( [ 'message' => 'User already has this Recipe' ] );
+    }
+
+    public function read( Request $request )
+    {
+        $userRecipe = DB::connection('mongodb')
+            ->collection('userRecipes')
+            ->where('userId', $request->userId)
+            ->first();
+
+        // Ensure you have retrieved the userRecipe record with the _id field.
+
+        if ($userRecipe) {
+            // Use the aggregation framework to join the userRecipe with the recipes table.
+            $result = DB::connection('mongodb')
+                ->collection('recipes')
+                ->raw(function ($collection) use ($userRecipe) {
+                    return $collection->aggregate([
+                        [
+                            '$match' => [
+                                'recipeId' => $userRecipe->_id
+                            ]
+                        ],
+                        [
+                            '$lookup' => [
+                                'from' => 'userRecipes',
+                                'localField' => '_id',
+                                'foreignField' => 'recipeId',
+                                'as' => 'userRecipes'
+                            ]
+                        ]
+                    ]);
+                })
+            ;
+            return response()->json($result);
+        }
+
+        return response()->json(['message' => 'User Recipes not found'], 404);
+    }
+
+    public function destroy( Request $request )
+    {
+        // Delete a user from the 'users' collection based on ID
+        $userRecipe =
+            DB::connection('mongodb')
+                ->collection('userRecipes')
+                ->where('userId', $userId)
+                ->where('recipeId', $recipeId)
+                ->delete();
+
+        return response()->json(['message' => 'User Recipe deleted']);
+    }
+}
--- Laravel-Backend/routes/api.php
+++ Laravel-Backend/routes/api.php
@@ -3,6 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApiController;
+use App\Http\Controllers\Users;
+use App\Http\Controllers\Ingredients;
+use App\Http\Controllers\Recipes;
+use App\Http\Controllers\UserRecipes;
+use App\Http\Controllers\UserIngredients;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,10 +23,65 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/login', [ApiController::class, 'login']);
-Route::post('/login', [ApiController::class, 'login']);
-Route::get('/registration', [ApiController::class, 'register']);
-Route::post('/registration', [ApiController::class, 'register']);
+Route::get('/login', [Users::class, 'login']);
+Route::post('/login', [Users::class, 'login']);
+
+Route::get('/user-create', [Users::class, 'create']);
+Route::get('/user-read', [Users::class, 'read']);
+Route::get('/user-update', [Users::class, 'update']);
+Route::get('/user-destroy', [Users::class, 'destroy']);
+
+Route::post('/user-create', [Users::class, 'create']);
+Route::post('/user-read', [Users::class, 'read']);
+Route::post('/user-update', [Users::class, 'update']);
+Route::post('/user-destroy', [Users::class, 'destroy']);
+
+
+Route::get('/ingredient-create', [Ingredients::class, 'create']);
+Route::get('/ingredient-read', [Ingredients::class, 'read']);
+Route::get('/ingredient-update', [Ingredients::class, 'update']);
+Route::get('/ingredient-destroy', [Ingredients::class, 'destroy']);
+
+Route::post('/ingredient-create', [Ingredients::class, 'create']);
+Route::post('/ingredient-read', [Ingredients::class, 'read']);
+Route::post('/ingredient-update', [Ingredients::class, 'update']);
+Route::post('/ingredient-destroy', [Users::class, 'destroy']);
+
+
+Route::get('/recipe-create', [Recipes::class, 'create']);
+Route::get('/recipe-read', [Recipes::class, 'read']);
+Route::get('/recipe-update', [Recipes::class, 'update']);
+Route::get('/recipe-destroy', [Recipes::class, 'destroy']);
+
+Route::post('/recipe-create', [Recipes::class, 'create']);
+Route::post('/recipe-read', [Recipes::class, 'read']);
+Route::post('/recipe-update', [Recipes::class, 'update']);
+Route::post('/recipe-destroy', [Recipes::class, 'destroy']);
+
+
+Route::get('/user-recipe-create', [UserRecipes::class, 'create']);
+Route::get('/user-recipe-read', [UserRecipes::class, 'read']);
+Route::get('/user-recipe-update', [UserRecipes::class, 'update']);
+Route::get('/user-recipe-destroy', [UserRecipes::class, 'destroy']);
+
+Route::post('/user-recipe-create', [UserRecipes::class, 'create']);
+Route::post('/user-recipe-read', [UserRecipes::class, 'read']);
+Route::post('/user-recipe-update', [UserRecipes::class, 'update']);
+Route::post('/user-recipe-destroy', [UserRecipes::class, 'destroy']);
+
+
+Route::get('/user-ingredient-create', [UserIngredients::class, 'create']);
+Route::get('/user-ingredient-read', [UserIngredients::class, 'read']);
+Route::get('/user-ingredient-update', [UserIngredients::class, 'update']);
+Route::get('/user-ingredient-destroy', [UserIngredients::class, 'destroy']);
+
+Route::post('/user-ingredient-create', [UserIngredients::class, 'create']);
+Route::post('/user-ingredient-read', [UserIngredients::class, 'read']);
+Route::post('/user-ingredient-update', [UserIngredients::class, 'update']);
+Route::post('/user-ingredient-destroy', [UserIngredients::class, 'destroy']);
+
+
 Route::get('/data', [ApiController::class, 'getData']);
-Route::get('/ingredients', [ApiController::class, 'getAllIngredients']);
+Route::get('/allIngredients', [ApiController::class, 'getAllIngredients']);
 Route::get('/allUsers', [ApiController::class, 'getAllUsers']);
+Route::get('/allRecipes', [ApiController::class, 'getAllRecipes']);
