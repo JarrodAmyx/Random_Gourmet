026e042a31d160aba3fd4b83d46ae88144e24811
Brian Tran <ninjapika500@outlook.com>
2023-11-06 19:00:04 -0800

    Recipe list works but doesnt work in rows

--- gourmet-angular/src/app/recipecard-list/recipecard-list.component.ts
+++ gourmet-angular/src/app/recipecard-list/recipecard-list.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+export interface recipe {
+  recipeID: number,
+  title: string,
+  ingredientIDs: number[],
+  description: string
+}
 
 @Component({
   selector: 'app-recipecard-list',
   templateUrl: './recipecard-list.component.html',
   styleUrls: ['./recipecard-list.component.css']
 })
-export class RecipecardListComponent {
+export class RecipecardListComponent implements OnInit{
+  @Input() _dataset: Array<recipe> = []
 
+  constructor(
+
+  ) { }
+
+  ngOnInit(): void {
+
+  }
 }
