4303175ad0ecdfcc99e315d76e19ac8f1c4a319f
JoakimEckerman <joakim.eckerman01@student.csulb.edu>
2023-11-09 20:54:53 -0800

    pantry

--- gourmet-angular/src/app/pantry/pantry.component.ts
+++ gourmet-angular/src/app/pantry/pantry.component.ts
@@ -8,21 +8,71 @@ import { HttpClient } from '@angular/common/http';
 })
 export class PantryComponent implements OnInit {
   pantryItems: any[] = [];
+  categorizedPantryItems: { category: string, items: any[] }[] = [];
+  selectedItem: any = {};
+  searchQuery: string = '';
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
     // Make an HTTP GET request to fetch data from your MongoDB database
-    this.http.get<any[]>('http://54.183.139.183/api/ingredients').subscribe(data => {
+    this.http.get<any[]>('http://54.183.139.183/api/allIngredients').subscribe(data => {
       this.pantryItems = data;
+      this.categorizePantryItems();
+    });
+  }
+
+  categorizePantryItems() {
+    // Categorize the pantry items
+    const categories = new Set(this.pantryItems.map(item => item.category));
+    this.categorizedPantryItems = [...categories].map(category => {
+      return {
+        category,
+        items: this.pantryItems.filter(item => item.category === category)
+      };
     });
   }
 
   onSearch(event: any) {
-    // Implement search functionality
+    const searchTerm = event.target.value.toLowerCase();
+
+    // Filter the categorized pantry items based on the search term
+    this.categorizedPantryItems = this.categorizedPantryItems.map(categoryGroup => {
+      return {
+        category: categoryGroup.category,
+        items: categoryGroup.items.filter(item =>
+          item.name.toLowerCase().includes(searchTerm)
+        )
+      };
+    });
   }
 
   addToIngredients(item: any) {
-    // Implement adding the selected item to a separate database or list
+    // Send a POST request to add the selected item
+    this.http.post('http://54.183.139.183/api/user-ingredient-create', item).subscribe(
+      (response) => {
+        // Handle success, for example, update the UI or show a success message
+        console.log('Item added successfully:', response);
+
+        // Optionally, you can update the local list if needed
+        this.categorizePantryItems();
+      },
+      (error) => {
+        // Handle the error, for example, show an error message
+        console.error('Error adding item:', error);
+      });
   }
+
+  toggleItemSelection(item: any) {
+    item.selected = !item.selected; // Toggle the selected property
+    if (item.selected) {
+      this.addToIngredients(item); // Add the selected item to your database/list
+    }
+  }
+
+  // Method to update the search query
+  onSearchInputChange(event: Event): void {
+    this.searchQuery = (event.target as HTMLInputElement).value.toLowerCase();
+  }
+
 }
