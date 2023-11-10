68183c80203ce829e12d06cfd22f127e36868853
JoakimEckerman <joakim.eckerman01@student.csulb.edu>
2023-11-09 20:54:35 -0800

    routing

--- gourmet-angular/src/app/auth/auth.guard.ts
+++ gourmet-angular/src/app/auth/auth.guard.ts
@@ -1,21 +1,28 @@
-import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Injectable, inject } from '@angular/core';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, Router, CanActivateFn } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
+import { SharedService } from '../shared/shared.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard {
+export class AuthGuardService {
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private authService: AuthService,
+    private sharedService: SharedService
+    ) {}
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | boolean {
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
     if (this.authService.isLoggedIn()) {
       return true; // User is authenticated, allow access
     } else {
-      this.router.navigate(['/login']); // Redirect to the login page if not authenticated
+      this.sharedService.openLogin(); // Redirect to the login page if not authenticated
       return false; // Prevent access to the route
     }
   }
+}
+export const AuthGuard: CanActivateFn = (next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean => {
+  return inject(AuthGuardService).canActivate(next, state);
 }
