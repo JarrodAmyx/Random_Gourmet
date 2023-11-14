e0d622790d17b778af58a2d1418f39a876624b35
JoakimEckerman <joakim.eckerman01@student.csulb.edu>
2023-11-13 20:22:18 -0800

    added a navigation folder for structure

--- gourmet-angular/src/app/auth/registration/registration.component.ts
+++ gourmet-angular/src/app/auth/registration/registration.component.ts
@@ -14,9 +14,6 @@ import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validator
 })
 
 export class RegistrationComponent {
-  Email: string = '';
-  password: string = '';
-  confirmPassword: string = '';
   errorMessage: string = '';
 
   registerForm : FormGroup;
@@ -29,7 +26,7 @@ export class RegistrationComponent {
     public dialogRef: MatDialogRef<RegistrationComponent>, 
     @Inject(MAT_DIALOG_DATA) public data: any) {
       this.registerForm = new FormGroup({
-        email: new FormControl('', [Validators.required, this.whiteSpace()]),
+        email: new FormControl('', [Validators.required]),
         password1: new FormControl('', [Validators.required, Validators.pattern('^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$'), Validators.minLength(8)]),
         password2: new FormControl('', [Validators.required, this.passwordDuplicateValid()]),
       }); 
@@ -43,27 +40,32 @@ export class RegistrationComponent {
   }
 
   onSubmit(): void {
-    this.authService.register(this.Email, this.password).subscribe(
-      (response) => {
-        // Successful registration
-        // You can choose to navigate to another page or display a success message.
-        // Automatically log in the user after successful registration
-        this.authService.login(this.Email, this.password).subscribe(
-          (loginResponse) => {
-            // Successful login
-            // Store the token, navigate to another page, or perform other actions
-          },
-          (loginError) => {
-            // Handle login error if necessary
-          }
-        );
-        this.dialogRef.close();
-      },
-      (error) => {
-        // Handle registration error
-        this.errorMessage = 'Registration failed';
-      }
-    );
+    const email = this.getEmail()?.value;
+    const password1 = this.getPassword1()?.value;
+  
+    if (email && password1) {
+      this.authService.register(email, password1).subscribe(
+        (response) => {
+          // Successful registration
+          // You can choose to navigate to another page or display a success message.
+          // Automatically log in the user after successful registration
+          this.authService.login(email, password1).subscribe(
+            (loginResponse) => {
+              // Successful login
+              // Store the token, navigate to another page, or perform other actions
+            },
+            (loginError) => {
+              // Handle login error if necessary
+            }
+          );
+          this.dialogRef.close();
+        },
+        (error) => {
+          // Handle registration error
+          this.errorMessage = 'Registration failed';
+        }
+      );
+    }
   }
 
 
@@ -89,15 +91,15 @@ export class RegistrationComponent {
     };
   }
 
-  get email(){
+  getEmail(){
     return this.registerForm.get('email');
   }
 
-  get password1(){
+  getPassword1(){
     return this.registerForm.get('password1');
   }
 
-  get password2(){
+  getPassword2(){
     return this.registerForm.get('password2');
   }
 }
