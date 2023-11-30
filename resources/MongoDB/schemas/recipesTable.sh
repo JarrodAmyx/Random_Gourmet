5b13384186072c0b62c3a4146e1848f73dfc1948
JarrodAmyx <125937385+JarrodAmyx@users.noreply.github.com>
2023-11-29 21:18:07 -0800

    Update recipesTable.sh

--- resources/MongoDB/schemas/recipesTable.sh
+++ resources/MongoDB/schemas/recipesTable.sh
@@ -4,7 +4,7 @@ db.createCollection("recipes", {
   validator: {
     $jsonSchema: {
       bsonType: "object",
-      required: ["recipeId", "title", "description", "requiredIngredients"],
+      required: ["recipeId", "title", "description", "requiredIngredients", "recipeImage"], // Added "recipeImage" to the required fields
       properties: {
         recipeId: { bsonType: "string" },
         title: { bsonType: "string" },
@@ -15,6 +15,10 @@ db.createCollection("recipes", {
             bsonType: "string" // References to ingredients in the "ingredients" table
           },
         },
+        recipeImage: {
+          bsonType: "string" // Assuming a string data type for storing image URLs or references
+          // You might consider using a different data type or a different approach based on your image storage method
+        }
       },
     },
   },
