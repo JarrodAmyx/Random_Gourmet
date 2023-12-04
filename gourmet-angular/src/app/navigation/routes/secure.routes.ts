80917bfc3a5d8926da2d810249170ae25e3a62d7
JoakimEckerman <joakim.eckerman01@student.csulb.edu>
2023-12-04 00:40:00 -0800

    saved recipes

--- gourmet-angular/src/app/navigation/routes/secure.routes.ts
+++ gourmet-angular/src/app/navigation/routes/secure.routes.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProfileComponent } from '../../profile/profile.component';
 import { PantryComponent } from '../../pantry/pantry.component';
 import { HomeComponent } from '../../home/home.component';
+import { SavedComponent } from '../../saved/saved.component';
 
 export const SECURE_ROUTES: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'profile', component: ProfileComponent },
-  { path: 'pantry', component: PantryComponent }
+  { path: 'pantry', component: PantryComponent },
+  { path: 'saved', component: SavedComponent }
 ];
